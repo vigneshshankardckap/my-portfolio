39e36518d01a9fdb80f94c914d3c6683d752c952
vigneshshankardckap <vigneshshankardckap@gmail.com>
2023-06-07 16:17:59 +0530

    my own site

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+const sr=scrollReveal({
+    distance:'45px',
+    duration:2700,
+    reset:true
+})
+sr.reaveal('.home-text',{delay:350,origin:'left'})
+sr.reaveal('.home-img',{delay:350,origin:'right'})
